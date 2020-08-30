0cc31653e30a50a3aedb94c3c844176351082561
andrei.tazetdinov <andrei.tazetdinov@dynatrace.com>
2020-08-30 14:47:10 +0200

    get data form file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const port = 8000;
 const fs = require('fs');
+let ejs = require('ejs');
 
 const changeDataInFile = (filename, dataArray) => {
     let obj = {};
@@ -28,11 +29,17 @@ app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 
 router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname + '/public/index.html'));
+  const filename = 'rules.conf';
+  fs.readFile(path.join(__dirname + '/init/'+ filename), 'utf8', function (err, data) {
+    if (err) { throw err; }
+    // данные получены в виде объекта JSON
+    res.render(path.join(__dirname + '/public/index.ejs'), JSON.parse(data.toString().trim()) );
+  });
+  
   //__dirname : It will resolve to your project folder.
 });
 
-router.post('/submit',function(req,res){
+router.post('/',function(req,res){
   console.log('[got data]', req.body.status);
   changeDataInFile('rules.conf', req.body)
   res.send('fine');
